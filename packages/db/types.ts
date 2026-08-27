15a5b25f705cbb1d80cf372644d9728794eb7d52
manab-debnath <manab_debnath@outlook.com>
2026-08-27 15:31:41 +0530

    export prisma schema model types

---
+++ packages/db/types.ts
@@ -0,0 +1,15 @@
+export type {
+  User,
+  Session,
+  Account,
+  Verification,
+  Organization,
+  OrganizationUser,
+  Board,
+  Issue,
+  Section,
+  IssueUser,
+  Comment
+} from "./generated/prisma/client";
+
+export {Role, IssueStatus} from "./generated/prisma/client";
