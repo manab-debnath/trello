07cf79446d0dca874724612581adca6b3e7be423
manab-debnath <manab_debnath@outlook.com>
2026-09-03 23:32:34 +0530

    refactor: simplify bullmq worker

--- apps/worker/index.ts
+++ apps/worker/index.ts
@@ -1,7 +1,6 @@
 import { Worker } from "bullmq";
 import IORedis from "ioredis";
 import { createLogger } from "logger";
-import type { EmailType } from "types";
 import { sendResendEmail } from "./config/resend";
 
 const connection = new IORedis({
@@ -13,26 +12,7 @@ export const logger = createLogger("worker");
 const worker = new Worker(
   "email",
   async (job) => {
-    switch (job.name as EmailType) {
-      case "EMAILVERIFICATION":
-        await sendResendEmail(
-          job.data.emailHeader,
-          job.data.user,
-          job.data.url,
-          job.name as EmailType,
-        );
-        break;
-      case "RESETPASSWORD":
-        await sendResendEmail(
-          job.data.emailHeader,
-          job.data.user,
-          job.data.url,
-          job.name as EmailType,
-        );
-        break;
-      default:
-        break;
-    }
+    await sendResendEmail(job.data);
   },
   {
     connection,
