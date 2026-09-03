9ae10114db36e589a7ca428aa104cd6fb6411d51
manab-debnath <manab_debnath@outlook.com>
2026-09-03 23:30:49 +0530

    add: EmailJob type

--- packages/types/type.ts
+++ packages/types/type.ts
@@ -1,9 +1,29 @@
+import type { User, Organization } from "db/types";
+
 interface EmailHeader {
   to: string;
   from: string;
   subject: string;
 }
 
-type EmailType = "EMAILVERIFICATION" | "RESETPASSWORD";
+type EmailJob =
+  | {
+      type: "EMAILVERIFICATION";
+      emailHeader: EmailHeader;
+      user: User;
+      url: string;
+    }
+  | {
+      type: "RESETPASSWORD";
+      emailHeader: EmailHeader;
+      user: User;
+      url: string;
+    }
+  | {
+      type: "SENDINVITATION";
+      emailHeader: EmailHeader;
+      user: User;
+      organization: Organization;
+    };
 
-export type { EmailHeader, EmailType };
+export type { EmailHeader, EmailJob };
