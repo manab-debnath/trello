46cac960f4c4bb10591963c83380cb091cf6149f
manab-debnath <manab_debnath@outlook.com>
2026-09-23 23:00:55 +0530

    add basic websocket structure in frontend

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes, useParams } from "react-router";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
@@ -13,11 +14,41 @@ function App() {
 
 function Board() {
   const { boardID } = useParams();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const ws = new WebSocket(`ws://localhost:8080/boards/${boardID}`);
+    ws.onmessage = (event: MessageEvent) => {
+      const data = JSON.parse(event.data);
+      if (data.type === "initial_state") {
+        setUsers(data.users);
+      }
+      if (data.type === "join") {
+        setUsers([...users, { id: data.userID }]);
+      }
+      if (data.type === "leave") {
+        setUsers(users.filter((user) => user.id !== data.userID));
+      }
+    };
+
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          type: "join",
+          boardID,
+        }),
+      );
+    };
+  }, [boardID, users]);
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-red-600">
         Board - {boardID}
       </h1>
+      <h3 className="font-bold">
+        Currently connected users: {JSON.stringify(users)}
+      </h3>
     </div>
   );
 }
